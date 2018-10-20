0d3bafe40c4fdd7f930bcf4b58be8273f883a57a
PavelAkulich <pavelakulic@gmail.com>
2018-10-20 12:30:22 +0300

    Finish

---
+++ OwnStack/OwnStack/OwnStack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnStack
+{
+    class OwnStack
+    {
+        static void Main(string[] args)
+        {
+            try
+
+            {
+                IntStack stack = new IntStack();
+                for (int i = 0; i < 11; i++)
+                {
+                    stack.Push(i);
+                }
+                for (int i = 0; i < 11; i++)
+                {
+                    Console.WriteLine(stack.arrStack[i]);
+                }
+                Console.WriteLine();
+                Console.WriteLine(stack.Pop());
+                Console.WriteLine();
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(stack.arrStack[i]);
+                }
+                Console.Read();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error: stack is empty");
+                Console.Read();
+            }
+            
+        }
+    }
+}
